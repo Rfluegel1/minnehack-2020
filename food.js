94deb83b4a1226c696dfe7e36f18b6a4312cee3f
azhulogic <43792633+azhulogic@users.noreply.github.com>
2020-01-26 01:56:05 -0600

    add printExpiryDate() and printPurchaseDate()

--- food.js
+++ food.js
@@ -1,71 +1,85 @@
 // food class
 // TODO:
-
 class Food {
     constructor(name) {
         this.foodname = name
         // defaults to current day in UTC, sets to CST
-        this.dateOfPurchase = new Date()
-        this.dateOfPurchase.setTime(this.dateOfPurchase.getTime() - 21600000)
+        this.PurchaseDate = new Date()
+        this.PurchaseDate.setTime(this.PurchaseDate.getTime() - 21600000)
 
         // TODO: incorporate default dictionary
         // right now defaulting to 7 days
-        this.dateOfExpiry = new Date()
-        this.dateOfExpiry.setTime(this.dateOfExpiry.getTime() - 21600000)
-        this.dateOfExpiry.setDate(this.dateOfExpiry.getDate() + 7)
+        this.ExpiryDate = new Date()
+        this.ExpiryDate.setTime(this.ExpiryDate.getTime() - 21600000)
+        this.ExpiryDate.setDate(this.ExpiryDate.getDate() + 7)
     }
-    getDateOfExpiry() {
+    getExpiryDate() {
         // we can return as Date object or string
-        // console.log(this.dateOfExpiry)
-        return this.dateOfExpiry
+        // console.log(this.ExpiryDate)
+        return this.ExpiryDate
     }
-    setDateOfExpiry(date) {
+    setExpiryDate(date) {
         // accepts input [year, month, day]
         // TODO: error checking
-        this.dateOfExpiry = new Date(date[0], date[1], date[2])
+        this.ExpiryDate = new Date(date[0], date[1], date[2])
     }
-    getDateOfPurchase() {
+    getPurchaseDate() {
         // we can return as Date object or string
-        // console.log(this.dateOfPurchase)
-        return this.dateOfPurchase
+        // console.log(this.PurchaseDate)
+        return this.PurchaseDate
     }
-    setDateOfPurchase(date) {
+    setPurchaseDate(date) {
         // [year, month, day]
         // TODO: error checking
-        this.dateOfPurchase = new Date(date[0], date[1], date[2])
+        this.PurchaseDate = new Date(date[0], date[1], date[2])
     }
-
     daysTilExpiry() {
         // returns days until expiration, -1 if expired
-        var daysTil = (this.dateOfExpiry - this.dateOfPurchase)/(1000*60*60*24)
+        var daysTil = (this.ExpiryDate - this.PurchaseDate)/(1000*60*60*24)
         if (daysTil < 0) {
             return -1
         } else {
             return daysTil
         }
     }
-}
-
-function sortAlpha(foodList) {
-    // takes a list of Foods, returns a sorted list
-    foodList.sort(function(a, b) {
-        if (a.foodname < b.foodname) { return -1 }
-        if (a.foodname > b.foodname) { return 1 }
-        return 0;
-    })
-    return foodList.sort()
+    printPurchaseDate() {
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday"]
+        var months = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"]
+        var dateString = ""
+        dateString += days[this.PurchaseDate.getDay()] + ", "
+        dateString += months[this.PurchaseDate.getMonth()] + " "
+        dateString += this.PurchaseDate.getDate()
+        return dateString
+    }
+    printExpiryDate() {
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday"]
+        var months = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"]
+        var dateString = ""
+        dateString += days[this.ExpiryDate.getDay()] + ", "
+        dateString += months[this.ExpiryDate.getMonth()] + " "
+        dateString += this.ExpiryDate.getDate()
+        return dateString
+    }
 }
 
 // // testing
 // var f = new Food()
 //
-// console.log(f.getDateOfPurchase())
-// console.log(f.getDateOfExpiry())
+// console.log(f.getPurchaseDate())
+// console.log(f.getExpiryDate())
 //
-// f.setDateOfPurchase([1999, 9, 1])
-// console.log(f.getDateOfPurchase())
-// f.setDateOfExpiry([2010, 2, 1])
-// console.log(f.getDateOfExpiry())
+// console.log(f.printPurchaseDate())
+// console.log(f.printExpiryDate())
+
+//
+// f.setPurchaseDate([1999, 9, 1])
+// console.log(f.getPurchaseDate())
+// f.setExpiryDate([2010, 2, 1])
+// console.log(f.getExpiryDate())
 //
 // console.log(f.daysTilExpiry())
 //
@@ -81,8 +95,8 @@ function sortAlpha(foodList) {
 //
 // function sortByExpiryDec(foodList) {
 //     foodList.sort(function(a, b) {
-//         if (a.dateOfExpiry < b.dateOfExpiry) { return -1 }
-//         if (a.dateOfExpiry > b.dateOfExpiry) { return 1 }
+//         if (a.ExpiryDate < b.ExpiryDate) { return -1 }
+//         if (a.ExpiryDate > b.ExpiryDate) { return 1 }
 //         return 0
 //     })
 //     return foodList
@@ -96,9 +110,9 @@ function sortAlpha(foodList) {
 // var b = new Food("Banana")
 // var c = new Food("Carrot")
 //
-// a.setDateOfExpiry([2000, 1, 1])
-// b.setDateOfExpiry([2001, 1, 1])
-// c.setDateOfExpiry([2002, 1, 1])
+// a.setExpiryDate([2000, 1, 1])
+// b.setExpiryDate([2001, 1, 1])
+// c.setExpiryDate([2002, 1, 1])
 //
 // var fridge = [c, a, b]
 // console.log(fridge)
